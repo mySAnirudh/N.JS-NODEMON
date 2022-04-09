c36372750693b81bc61d2f182498ca71ae81dd0d
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-04-10 00:06:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+console.warn("Hello Samrat") // yaha hame baar baar run karna parda hai but jab ham nodemoon use karange 
+// to ek baad me hi kaam ho jaayega
+
+
+// After installing node modmon jaise ham kauch bi likhate hai automatic save karke update kar deta hai
